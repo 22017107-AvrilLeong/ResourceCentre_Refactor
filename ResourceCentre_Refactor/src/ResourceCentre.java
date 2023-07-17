56e3f390aa7aea8d1d5d5857fcc2a452df120015
JocelynLee <JocelynLee@Jocelyn>
2023-07-17 16:26:20 +0800

    jocelyn comment

--- ResourceCentre_Refactor/src/ResourceCentre.java
+++ ResourceCentre_Refactor/src/ResourceCentre.java
@@ -17,7 +17,7 @@ public class ResourceCentre {
 
 		int option = 0;
 
-		while (option != OPTION_QUIT) {//jocelyn
+		while (option != OPTION_QUIT) {//jocelyn comment
 
 			ResourceCentre.menu();
 			option = Helper.readInt("Enter an option > ");
